5f401e5b4939106e370af79575ae6a88378e8eee
kozaru <kozaru@corogari.com>
2017-11-03 15:14:58 +0900

    Add Scene3 People Animation #3

--- Assets/Scripts/People/SpScene3.cs
+++ Assets/Scripts/People/SpScene3.cs
@@ -10,7 +10,7 @@ public class SpScene3 : MonoBehaviour {
 	void Start () {
 		this.animator = this.GetComponent<Animator>();
 		int x = 0;
-		x = Random.Range(1,4);
+		x = Random.Range(1,18);
 		this.animator.SetInteger("Movement_int",x);
 	}
 }
